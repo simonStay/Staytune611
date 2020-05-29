af2485202e9cf382903dc391687f88c40a223f31
Liathe Najdawi <lnajdawi@memphis.edu>
2020-05-29 15:30:22 -0500

    Elvis Implementation

---
+++ app/screens/chat-elvis/steps.js
@@ -0,0 +1,145 @@
+export default customSteps = {
+    "filterType": {
+        "title": "Here’s the initial categories you can choose from and I’ll start providing some recommendations. Start by selecting one from this list.",
+        "filter": [
+            // {
+            //     "label": "Hotels"
+            // },
+            {
+                "label": "Restaurants"
+            },
+            {
+                "label": "Activities"
+            },
+            {
+                "label": "Bar & Live music"
+            },
+            {
+                "label": "Beauty & Health"
+            },
+            {
+                "label": "Shops"
+            },
+            {
+                "label": "Specialty"
+            }
+      ]
+    },
+    "HotelsType": {
+        "title": "Here is some famous Hotels!!!",
+        "Hotels": [{
+            "label": "Four Seasons"
+        },
+        {
+            "label": "Heywood Hotel"
+        },
+        {
+            "label": "Hotel Ella"
+        }
+        ]
+    },
+    "RestaurantsType": {
+        "title": "Here is some famous Restaurants!!!",
+        "Restaurants": [{
+            "label": "The Arcade"
+        },
+        {
+            "label": "Coletta's"
+        },
+        {
+            "label": "Catherine & Mary's"
+        },
+        {
+             "label" : "Johnnie's"
+         },
+         {
+             "label":"Galdy's Diner"
+         },
+         {
+             "label":"Nernon's Smokehouse"
+         }
+        ]
+    },
+    "ActivitiesType": {
+        "title": "Here is some famous Activities!!!",
+        "Activities": [{
+            "label": "Lauderdale Courts"
+        },
+        {
+            "label": "Sun Studio"
+        },
+        {
+            "label": "Graceland"
+        },
+        {
+            "label": "The Peabody Memphis"
+        },
+        {
+            "label": "Blues Hall of Fame"
+        }
+        ]
+    },
+    "Bar_LivemusicType": {
+        "title": "Here is some famous Bar & Live music!!!",
+        "Bar_Livemusic": [{
+            "label": "Beale Street"
+        },
+        {
+            "label": "Levit Shell"
+        },
+        {
+            "label": "Circuit Playhouse"
+        }
+        ]
+    },
+    "Beauty_HealthType": {
+        "title": "Here is some famous Beauty & Health!!!",
+        "Beauty_Health": [{
+            "label": "B-cycle"
+        },
+        {
+            "label": "Lake Austin Spa Resort"
+        },
+        {
+            "label": "Love Cycling Studio"
+        }
+        ]
+    },
+    "ShopsType": {
+        "title": "Here is some famous Shops!!!",
+        "Shops": [{
+            "label": "Lansky's"
+        },
+        {
+            "label": "Goner Records"
+        },
+        {
+            "label": "Flashback"
+        }
+        ]
+    },
+    "SpecialtyType": {
+        "title": "Here is some famous Specialities!!!",
+        "Specialty": [{
+            "label": "Amy's Ice Creams"
+        },
+        {
+            "label": "Boteco"
+        },
+        {
+            "label": "Bouldin Creek Cafe"
+        }
+        ]
+    },
+    "QuestionType": {
+        "title": "Any other recommendations you want me to provide,",
+        "Question": [{
+            "label": "Yes"
+        },
+        {
+            "label": "No"
+        }
+        ]
+    }
+
+};
